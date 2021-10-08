d33036bd853a2505932f51023d79a6ca5502d94a
asdofa <91516747+asdofa@users.noreply.github.com>
2021-10-08 22:32:01 +0700

    Update connection_connect.php

--- connection_connect.php
+++ connection_connect.php
@@ -1,11 +1,11 @@
 <?php
 
     $severname = "localhost";
-    $username = "db21_023";
-    $password ="db21_023";
-    $dbname = "db21_023";
+    $username = "db21_090";
+    $password ="db21_090";
+    $dbname = "db21_090";
     $conn = new mysqli($severname,$username,$password);
-    mysqli_set_charset($conn, "utf8");
+    mysqli_set_charset($conn, "utf8");//โซนเวลา
     if($conn->connect_error)
     {
         die("Connection failed: ".$conn->connect_error);

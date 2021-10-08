71e738e024bf3863fe1791c34d2fdf057d5473c7
asdofa <91516747+asdofa@users.noreply.github.com>
2021-10-08 23:10:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
     if(isset($_GET['controller']) && isset($_GET['action'])) 
-    {   
+    {
         $controller = $_GET['controller'];
         $action = $_GET['action'];
-    }else{   
+    }else{
         $controller= 'pages'; 
         $action = 'home';
     }

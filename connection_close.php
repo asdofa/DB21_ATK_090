c9626bc0c7e33c8900338f3eb705ef4dab703399
asdofa <91516747+asdofa@users.noreply.github.com>
2021-10-08 22:34:39 +0700

    Create connection_close.php

---
+++ connection_close.php
@@ -0,0 +1,10 @@
+
+<?php
+    $severname = "localhost";
+    $username = "db21_090";
+    $password ="db21_090";
+    $dbname = "db21_090";
+
+    $conn = mysqli_connect($severname,$username,$password);
+    $conn->close();
+?>

7144bbf0bc8d108947eddabe2674a518c9a4aec0
asdofa <91516747+asdofa@users.noreply.github.com>
2021-10-13 15:54:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,10 @@
     [<a href="?controller=item&action=index">Item</a>]
     [<a href="?controller=list_item&action=index">List_Item</a>]
     [<a href="?controller=record_assessment&action=index">Record_Assessment</a>]
-    echo "1";
     <br>
     <?php echo "controller= ".$controller.", action=".$action; ?>
-    <?php require_once("./routes.php"); ?>
+    <?php require_once("./routes.php"); 
+        echo "1";
+    ?>
 </body>
 </html>

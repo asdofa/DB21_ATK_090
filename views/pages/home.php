fb2f870ff2585199e2ea13c2be0e8ec926ac6212
asdofa <91516747+asdofa@users.noreply.github.com>
2021-11-10 16:49:43 +0700

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,18 +1,9 @@
 <html>
 <center>
     <style>
-        body {
-              background-color: #FFCDD2;
-              color: #eb8c4a;
-              font-family:FC Friday;
-              font-size: 25px;
-              text-align: center;
-               }
     </style>
     <body>
-    <br><br><br>
-        <img src="https://fontmeme.com/permalink/211004/980ff786175c726a858cb03af2568001.png" alt="cocomelon-font" border="0"><br><br>
-        <br><br><br>
+    <br><br>
     </body>
 </center>
 </html>
